a4686bef78fc188117c15dd700c6e3edac7dc659
Jaromir Svejda <Jaromir@DESKTOP-N17U6SJ>
2025-04-23 20:47:58 +0200

    ninth

--- classes/Database.class.php
+++ classes/Database.class.php
@@ -8,7 +8,7 @@ class Database {
         if(null == self::$instance){
             try {
                 //pripojeni k DB
-                self::$instance = new PDO('mysql:host=134.209.226.86:3306;dbname=68092f6e6c42adf121bf125b', '68092f6e6c42adf121bf125b', 'fb367dae1dc7');
+                self::$instance = new PDO('mysql:host=134.209.226.86:3306;dbname=6809314a6c42adf121bf126f', '6809314a6c42adf121bf126f', 'fb367dae1dc7');
                 //hlaseni chyb DB
                 self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
